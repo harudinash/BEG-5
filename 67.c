3fea6cf528bc95aaa455b18b76ce6205c9ea528a
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-10 22:21:45 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,i,j,k,r;
+    printf("Input : ");
+    scanf("%d",&a);
+    printf("Output :\n");
+     
+        r=a%7;
+        if(r==0)
+        {
+            printf("Yes");
+            
+        }
+        else
+        printf("No");
+    
+        
+    
+    return 0;
+}
