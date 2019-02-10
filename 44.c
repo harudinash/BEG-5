c8cf2d573cfadde4d67d439c558b2a954257a1a5
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-10 16:28:22 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int i,j,h,c=0;
+  
+  printf("Input : ");
+  scanf("%d",&h);
+  printf(" Output : \n");
+  
+  if((h>=1)&&(h<=10))
+  printf("Yes");
+  else
+  printf("No");
+
+    return 0;
+}
